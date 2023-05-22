d5eba7dc4ac73a7065ffe9e8ce6c1edfc9bfe999
Laura <laurafm@al.insper.edu.br>
2023-05-22 16:41:58 -0300

    gametrue

--- teste.py
+++ teste.py
@@ -9,3 +9,10 @@ window = pygame.display.set_mode((largura, altura))
 fundo = pygame.image.load('Pasta/Fundo.png').convert()
 fundo = pygame.transform.scale(fundo, (600, 480))
 pygame.init()
+
+#Tela 
+window = pygame.display.set_mode((600, 480))
+pygame.display.set_caption('Jornada da Princesa')
+
+#GAMETRUE
+game = True
