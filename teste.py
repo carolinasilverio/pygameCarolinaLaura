aa6fa784b196968ac281df8e5574720c899a77af
Laura <laurafm@al.insper.edu.br>
2023-05-22 17:04:02 -0300

    espaco

--- teste.py
+++ teste.py
@@ -10,7 +10,6 @@ altura = 400
 window = pygame.display.set_mode((largura, altura))
 fundo = pygame.image.load('Pasta/Fundo.png').convert()
 fundo = pygame.transform.scale(fundo, (600, 480))
-pygame.init()
 
 #Tela 
 window = pygame.display.set_mode((600, 480))
